0c272704d23b94c60622fc607af7704a03814276
P1R0 <piro.stan@gmail.com>
2013-10-29 09:16:16 -0700

    Agregamos Examen III que funciona bien y el inicio para el examen I

--- Examen III/Examen III/Form1.Designer.cs
+++ Examen III/Examen III/Form1.Designer.cs
@@ -30,7 +30,7 @@
         {
             this.A = new System.Windows.Forms.DataGridView();
             this.B = new System.Windows.Forms.DataGridView();
-            this.C = new System.Windows.Forms.DataGridView();
+            this.P = new System.Windows.Forms.DataGridView();
             this.tamaño = new System.Windows.Forms.Button();
             this.filas = new System.Windows.Forms.TextBox();
             this.columnas = new System.Windows.Forms.TextBox();
@@ -40,9 +40,11 @@
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.calcular = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.A)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.B)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.C)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.P)).BeginInit();
             this.SuspendLayout();
             // 
             // A
@@ -61,20 +63,20 @@
             this.B.Size = new System.Drawing.Size(240, 211);
             this.B.TabIndex = 1;
             // 
-            // C
+            // P
             // 
-            this.C.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.C.Location = new System.Drawing.Point(736, 48);
-            this.C.Name = "C";
-            this.C.Size = new System.Drawing.Size(240, 211);
-            this.C.TabIndex = 2;
+            this.P.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.P.Location = new System.Drawing.Point(736, 48);
+            this.P.Name = "P";
+            this.P.Size = new System.Drawing.Size(240, 211);
+            this.P.TabIndex = 2;
             // 
             // tamaño
             // 
-            this.tamaño.Font = new System.Drawing.Font("Segoe Script", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
-            this.tamaño.Location = new System.Drawing.Point(208, 312);
+            this.tamaño.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tamaño.Location = new System.Drawing.Point(220, 322);
             this.tamaño.Name = "tamaño";
-            this.tamaño.Size = new System.Drawing.Size(98, 32);
+            this.tamaño.Size = new System.Drawing.Size(75, 23);
             this.tamaño.TabIndex = 3;
             this.tamaño.Text = "Tamaño";
             this.tamaño.UseVisualStyleBackColor = true;
@@ -82,11 +84,10 @@
             // 
             // filas
             // 
-            this.filas.Location = new System.Drawing.Point(163, 275);
+            this.filas.Location = new System.Drawing.Point(177, 276);
             this.filas.Name = "filas";
             this.filas.Size = new System.Drawing.Size(69, 20);
             this.filas.TabIndex = 4;
-            this.filas.TextChanged += new System.EventHandler(this.filas_TextChanged);
             // 
             // columnas
             // 
@@ -98,51 +99,50 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Segoe Script", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
-            this.label1.Location = new System.Drawing.Point(133, 274);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(147, 275);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 25);
+            this.label1.Size = new System.Drawing.Size(24, 16);
             this.label1.TabIndex = 6;
             this.label1.Text = "m:";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Segoe Script", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.Location = new System.Drawing.Point(277, 275);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(29, 25);
+            this.label2.Size = new System.Drawing.Size(20, 16);
             this.label2.TabIndex = 7;
             this.label2.Text = "n:";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Segoe Script", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.Location = new System.Drawing.Point(241, 13);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(25, 25);
+            this.label3.Size = new System.Drawing.Size(18, 16);
             this.label3.TabIndex = 8;
             this.label3.Text = "A";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Segoe Script", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.Location = new System.Drawing.Point(542, 13);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(40, 25);
+            this.label4.Size = new System.Drawing.Size(30, 16);
             this.label4.TabIndex = 9;
             this.label4.Text = "B^t";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Segoe Script", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label5.Location = new System.Drawing.Point(833, 13);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(25, 25);
+            this.label5.Size = new System.Drawing.Size(18, 16);
             this.label5.TabIndex = 10;
             this.label5.Text = "P";
             // 
@@ -154,13 +154,36 @@
             this.calcular.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Red;
             this.calcular.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Lime;
             this.calcular.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.calcular.Font = new System.Drawing.Font("Segoe Script", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
-            this.calcular.Location = new System.Drawing.Point(612, 312);
+            this.calcular.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.calcular.Location = new System.Drawing.Point(500, 317);
             this.calcular.Name = "calcular";
             this.calcular.Size = new System.Drawing.Size(200, 32);
             this.calcular.TabIndex = 11;
             this.calcular.Text = "Calcular";
             this.calcular.UseVisualStyleBackColor = false;
+            this.calcular.Click += new System.EventHandler(this.calcular_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Arimo", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(542, 368);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(0, 18);
+            this.label6.TabIndex = 12;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.DarkOrange;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("Palatino Linotype", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(721, 317);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(172, 32);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "Borrar Datos";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
@@ -168,6 +191,8 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Wheat;
             this.ClientSize = new System.Drawing.Size(1119, 420);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label6);
             this.Controls.Add(this.calcular);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
@@ -177,14 +202,14 @@
             this.Controls.Add(this.columnas);
             this.Controls.Add(this.filas);
             this.Controls.Add(this.tamaño);
-            this.Controls.Add(this.C);
+            this.Controls.Add(this.P);
             this.Controls.Add(this.B);
             this.Controls.Add(this.A);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.A)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.B)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.C)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.P)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -194,7 +219,7 @@
 
         private System.Windows.Forms.DataGridView A;
         private System.Windows.Forms.DataGridView B;
-        private System.Windows.Forms.DataGridView C;
+        private System.Windows.Forms.DataGridView P;
         private System.Windows.Forms.Button tamaño;
         private System.Windows.Forms.TextBox filas;
         private System.Windows.Forms.TextBox columnas;
@@ -204,6 +229,8 @@
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Button calcular;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button1;
     }
 }
 
---
+++ Examen III/Examen III/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Numerics;
+
+namespace Examen_III
+{
+    public partial class Form1 : Form
+    {
+        int m, n;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void tamaño_Click(object sender, EventArgs e)
+        {
+            m = Int32.Parse(filas.Text);
+            n = Int32.Parse(columnas.Text);
+            A.RowCount = m;
+            A.ColumnCount = n;
+            B.RowCount = n;
+            B.ColumnCount = m;
+            P.RowCount = m;
+            P.ColumnCount = n;
+        }
+
+        private void calcular_Click(object sender, EventArgs e)
+        {
+            Complex[,] X = new Complex[m, n];
+            string s, aux = "", aux2="";
+            double r, im;
+            int k;
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++) 
+                {
+                    s = A[j, i].Value.ToString();
+                    for (k = 0; k < s.Length; k++) 
+                    {
+                        if (s[k] != ',')
+                        {
+                            aux = aux + s[k];
+                        }
+                        else break;
+                    }
+                    for (int g = k + 1; g < s.Length; g++)
+                    {
+                        aux2 = aux2 + s[g];
+                    }
+                    Double.TryParse(aux, out r);
+                    Double.TryParse(aux2, out im);
+                    X[i, j] = new Complex(r, im);
+                    aux = "";
+                    aux2 = "";
+                }
+            }
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    B[j, i].Value = X[j,i];
+                }
+            }
+            if (m != n) 
+            {
+                label6.Text = "No se puede realizar la resta (condición: m = n).";
+            }
+            else
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    for (int j = 0; j < m; j++)
+                    {
+                        P[j, i].Value = Complex.Subtract(X[i, j], X[j, i]);
+                    }
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            filas.Text = "";
+            columnas.Text = "";
+            B.Rows.Clear();
+            B.Columns.Clear();
+            A.Rows.Clear();
+            A.Columns.Clear();
+            P.Rows.Clear();
+            P.Columns.Clear();
+        }
+    }
+}
